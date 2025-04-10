5e7bad7b1017b2047f691e1792498c21ba8fed2e
Simon Salvaing <simon.salvaing@gmail.com>
2025-04-10 17:45:17 +0200

    Add tests for Question model, and for index and detail views

--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -1,6 +1,7 @@
 from django.db.models import F
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, reverse
+from django.utils import timezone
 from django.views import generic
 
 from .models import Question, Choice
@@ -12,12 +13,16 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by("-pub_date")[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
+
+    def get_queryset(self):
+        """Excludes any question that are not published yet."""
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
